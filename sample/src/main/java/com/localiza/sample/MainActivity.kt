81bdb198f1113f8a559142f2979178faa900079a
Fernanda Olimpio Jesus de Abreu <zup04@localiza.com>
2023-02-28 18:11:09 +0000

    Feat: adding colors android
    Feat: adding colors android

---
+++ sample/src/main/java/com/localiza/sample/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.localiza.sample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.localiza.designsystem.theme.LDSColors
+import com.localiza.sample.ui.theme.DesignsystemTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DesignsystemTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = LDSColors.amber10
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DesignsystemTheme {
+        Greeting("Android")
+    }
+}
